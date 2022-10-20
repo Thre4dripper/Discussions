50b3c7f068122edfd2458a3348267299bcdbd6cf
Ijlal Ahmad <ijlalahmad845@gmail.com>
2022-10-20 16:36:40 +0530

    added login and signup fragment

--- app/src/main/java/com/example/discussions/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/discussions/ui/login/LoginActivity.kt
@@ -1,11 +1,12 @@
-package com.example.discussions
+package com.example.discussions.ui.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.discussions.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
