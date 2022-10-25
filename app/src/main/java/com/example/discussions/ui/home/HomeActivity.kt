fda7671dbff59ea43afaac75e85d7c89a9d69952
Ijlal Ahmad <ijlalahmad845@gmail.com>
2022-10-25 18:20:54 +0530

    added discuss and profile fragments, new google font and tested fragment transactions

--- app/src/main/java/com/example/discussions/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/discussions/ui/home/HomeActivity.kt
@@ -17,5 +17,41 @@ class HomeActivity : AppCompatActivity() {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
         binding.bottomNavigationView.background = null
+
+        initFragments()
+    }
+
+    private fun initFragments() {
+        val discussFragment = DiscussFragment()
+        val profileFragment = ProfileFragment()
+
+        //initial fragment
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, discussFragment).commit()
+
+        //bottom navigation listener
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.navigation_home -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, discussFragment).commit()
+                    true
+                }
+                R.id.navigation_poll -> {
+                    //TODO: add poll fragment
+                    true
+                }
+                R.id.navigation_notification -> {
+                    //TODO: add notification fragment
+                    true
+                }
+                R.id.navigation_profile -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, profileFragment).commit()
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
