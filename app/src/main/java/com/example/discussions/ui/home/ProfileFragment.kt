269abf9e076db387f190af013301286614d1ebb5
Ijlal Ahmad <ijlalahmad845@gmail.com>
2022-11-05 01:35:18 +0530

    refactored profile fragment and fixed dp image view

--- app/src/main/java/com/example/discussions/ui/home/ProfileFragment.kt
+++ app/src/main/java/com/example/discussions/ui/home/ProfileFragment.kt
@@ -5,14 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.discussions.R
+import com.bumptech.glide.Glide
+import com.example.discussions.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
+    lateinit var binding: FragmentProfileBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+
+        Glide.with(this)
+            .load("https://media1.popsugar-assets.com/files/thumbor/8su-YMfzKmaONa2ODYaWx7dltHI/fit-in/500x500/filters:format_auto-!!-:strip_icc-!!-/2018/07/16/749/n/1922398/4c32bf875b4ccef29a4812.53698795_/i/Tom-Cruise.jpg")
+            .into(binding.profileIv)
+
+        return binding.root
     }
 }
