62c1d00a311b3e4bffd759078e042128e3ea6b94
Ijlal Ahmad <ijlalahmad845@gmail.com>
2022-11-05 17:28:31 +0530

    added edit profile activity with design

--- app/src/main/java/com/example/discussions/ui/home/ProfileFragment.kt
+++ app/src/main/java/com/example/discussions/ui/home/ProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.example.discussions.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,6 +8,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.example.discussions.databinding.FragmentProfileBinding
+import com.example.discussions.ui.editProfile.EditProfileActivity
 
 class ProfileFragment : Fragment() {
     lateinit var binding: FragmentProfileBinding
@@ -20,6 +22,11 @@ class ProfileFragment : Fragment() {
         Glide.with(this)
             .load("https://media1.popsugar-assets.com/files/thumbor/8su-YMfzKmaONa2ODYaWx7dltHI/fit-in/500x500/filters:format_auto-!!-:strip_icc-!!-/2018/07/16/749/n/1922398/4c32bf875b4ccef29a4812.53698795_/i/Tom-Cruise.jpg")
             .into(binding.profileIv)
+
+        binding.editProfileBtn.setOnClickListener {
+            val intent = Intent(requireContext(), EditProfileActivity::class.java)
+            startActivity(intent)
+        }
 
         return binding.root
     }
