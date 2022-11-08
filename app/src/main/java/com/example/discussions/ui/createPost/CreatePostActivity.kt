fb5568e2339b2cb4484029645bcef2d1930b0d36
Ijlal Ahmad <ijlalahmad845@gmail.com>
2022-11-08 13:11:59 +0530

    added create post activity and triggered from bottom sheet

---
+++ app/src/main/java/com/example/discussions/ui/createPost/CreatePostActivity.kt
@@ -0,0 +1,12 @@
+package com.example.discussions.ui.createPost
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.discussions.R
+
+class CreatePostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_post)
+    }
+}
--- app/src/main/java/com/example/discussions/ui/home/CreatePostsBottomSheet.kt
+++ app/src/main/java/com/example/discussions/ui/home/CreatePostsBottomSheet.kt
@@ -1,18 +1,27 @@
 package com.example.discussions.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.discussions.R
+import com.example.discussions.databinding.PostsBsLayoutBinding
+import com.example.discussions.ui.createPost.CreatePostActivity
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
-class CreatePostsBottomSheet: BottomSheetDialogFragment() {
+class CreatePostsBottomSheet : BottomSheetDialogFragment() {
+    lateinit var binding: PostsBsLayoutBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.posts_bs_layout, container, false)
+    ): View {
+        binding = PostsBsLayoutBinding.inflate(inflater, container, false)
+
+        binding.createPostBtn.setOnClickListener {
+            startActivity(Intent(requireContext(), CreatePostActivity::class.java))
+            dismiss()
+        }
+        return binding.root
     }
 }
