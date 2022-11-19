543788a6da1c37b0ddf3714db552be078a1f90a0
Ijlal Ahmad <ijlalahmad845@gmail.com>
2022-11-19 23:47:28 +0530

    implemented fetch user image and username in create/edit poll activity

--- app/src/main/java/com/example/discussions/ui/CreateEditPollActivity.kt
+++ app/src/main/java/com/example/discussions/ui/CreateEditPollActivity.kt
@@ -1,12 +1,83 @@
 package com.example.discussions.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
+import com.example.discussions.Constants
 import com.example.discussions.R
+import com.example.discussions.databinding.ActivityCreateEditPollBinding
+import com.example.discussions.databinding.LoadingDialogBinding
+import com.example.discussions.viewModels.CreateEditPollViewModel
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class CreateEditPollActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCreateEditPollBinding
+    private lateinit var viewModel: CreateEditPollViewModel
+
+    private lateinit var loadingDialog: AlertDialog
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_create_edit_poll)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_create_edit_poll)
+        viewModel = ViewModelProvider(this)[CreateEditPollViewModel::class.java]
+
+        //initializing back button
+        binding.createPollBackBtn.setOnClickListener {
+            finish()
+        }
+
+        //initializing poll creation button
+        binding.createPollBtn.setOnClickListener {
+
+        }
+
+        //initializing user profile image click to zoom in button
+        binding.createPollProfileIv.setOnClickListener {
+            val intent = Intent(this, ZoomImageActivity::class.java)
+            intent.putExtra(Constants.ZOOM_IMAGE_URL, viewModel.profileImage)
+            startActivity(intent)
+        }
+
+        initLoadingDialog()
+        initUsernameAndImage()
+    }
+
+    /**
+     * METHOD FOR INITIALIZING LOADING DIALOG
+     */
+    private fun initLoadingDialog() {
+        val dialogBinding = LoadingDialogBinding.inflate(layoutInflater)
+        loadingDialog = MaterialAlertDialogBuilder(this).setView(dialogBinding.root)
+            .setCancelable(false).show()
+        loadingDialog.dismiss()
+    }
+
+    /**
+     * METHOD FOR INITIALIZING USERNAME AND PROFILE IMAGE
+     */
+    private fun initUsernameAndImage() {
+        loadingDialog.show()
+        viewModel.isApiFetched.observe(this) {
+            if (it != null) {
+                loadingDialog.dismiss()
+
+                if (it == Constants.API_SUCCESS) {
+                    Glide.with(this)
+                        .load(viewModel.profileImage)
+                        .placeholder(R.drawable.ic_profile)
+                        .circleCrop()
+                        .into(binding.createPollProfileIv)
+
+                    binding.createPollUsernameTv.text = viewModel.username
+                } else {
+                    Toast.makeText(this, "Error fetching user data", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+        viewModel.getUsernameAndImage(this)
     }
 }
---
+++ app/src/main/java/com/example/discussions/viewModels/CreateEditPollViewModel.kt
@@ -0,0 +1,36 @@
+package com.example.discussions.viewModels
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.discussions.Constants
+import com.example.discussions.api.ResponseCallback
+import com.example.discussions.repositories.UserRepository
+
+class CreateEditPollViewModel : ViewModel() {
+
+    var profileImage: String? = null
+    var username: String = ""
+    var pollTitle: String = ""
+    var pollContent: String = ""
+    var isPrivate: Boolean = true
+
+    private var _isApiFetched = MutableLiveData<String>(null)
+    val isApiFetched: LiveData<String>
+        get() = _isApiFetched
+
+    fun getUsernameAndImage(context: Context) {
+        UserRepository.getUsernameAndImage(context, object : ResponseCallback {
+            override fun onSuccess(response: String) {
+                UserRepository.map[Constants.PROFILE_IMAGE]?.let { profileImage = it }
+                UserRepository.map[Constants.USERNAME]?.let { username = it }
+                _isApiFetched.postValue(Constants.API_SUCCESS)
+            }
+
+            override fun onError(response: String) {
+                _isApiFetched.postValue(response)
+            }
+        })
+    }
+}
