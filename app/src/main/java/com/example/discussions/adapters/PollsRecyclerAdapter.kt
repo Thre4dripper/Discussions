4add8ef1e18f6c0ec0c789cc5c2134cb4d844144
Ijlal Ahmad <ijlalahmad845@gmail.com>
2022-11-24 16:14:11 +0530

    created list adapter for poll and displayed polls data and prepared to display poll options

--- app/src/main/java/com/example/discussions/adapters/PollsRecyclerAdapter.kt
+++ app/src/main/java/com/example/discussions/adapters/PollsRecyclerAdapter.kt
@@ -1,78 +1,124 @@
 package com.example.discussions.adapters
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.bumptech.glide.Glide
+import com.example.discussions.Constants
 import com.example.discussions.R
 import com.example.discussions.databinding.ItemDiscussionPollBinding
+import com.example.discussions.models.PollModel
+import com.example.discussions.ui.ZoomImageActivity
+import java.text.SimpleDateFormat
+import java.util.*
 
-class PollsRecyclerAdapter : RecyclerView.Adapter<PollsRecyclerAdapter.PollViewHolder>() {
+class PollsRecyclerAdapter : ListAdapter<PollModel, ViewHolder>(PollsDiffCallback()) {
 
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PollViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.item_discussion_poll, parent, false)
-
         return PollViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: PollViewHolder, position: Int) {
-        holder.bind(holder.binding)
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val poll = getItem(position)
+        (holder as PollViewHolder).bind(holder.binding, poll)
     }
 
-    override fun getItemCount() = 10
 
-    class PollViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    class PollViewHolder(itemView: View) : ViewHolder(itemView) {
         val binding = DataBindingUtil.bind<ItemDiscussionPollBinding>(itemView)!!
+        val pollOptionsTvList = mutableListOf(
+            binding.itemPollOption1Tv,
+            binding.itemPollOption2Tv,
+            binding.itemPollOption3Tv,
+            binding.itemPollOption4Tv,
+            binding.itemPollOption5Tv,
+            binding.itemPollOption6Tv,
+        ).toTypedArray()
 
-        fun bind(binding: ItemDiscussionPollBinding) {
-            //dummy data
-            binding.itemPostUsername.text = "Username"
-            binding.itemPostTime.text = "11:00 AM"
-            binding.itemPostTitle.text = "Programming is fun"
-            binding.itemPostContent.text = "What is your favorite programming language?"
-            binding.itemPollOption1.text = "C++"
-            binding.itemPollOption2.text = "Java"
-            binding.itemPollOption3.text = "Kotlin"
-            binding.itemPollOption4.text = "Python"
-            binding.itemPollOption5.text = "Javascript"
-            binding.itemPollOption6.text = "C#"
+        val pollOptionsResultLayoutList = mutableListOf(
+            binding.itemPollOption1Ll,
+            binding.itemPollOption2Ll,
+            binding.itemPollOption3Ll,
+            binding.itemPollOption4Ll,
+            binding.itemPollOption5Ll,
+            binding.itemPollOption6Ll,
+        ).toTypedArray()
 
-            binding.itemPollOption1Votes.text = "10%"
-            binding.itemPollOption2Votes.text = "20%"
-            binding.itemPollOption3Votes.text = "30%"
-            binding.itemPollOption4Votes.text = "40%"
-            binding.itemPollOption5Votes.text = "50%"
-            binding.itemPollOption6Votes.text = "60%"
+        val pollOptionsVotesTvList = mutableListOf(
+            binding.itemPollOption1Votes,
+            binding.itemPollOption2Votes,
+            binding.itemPollOption3Votes,
+            binding.itemPollOption4Votes,
+            binding.itemPollOption5Votes,
+            binding.itemPollOption6Votes,
+        ).toTypedArray()
 
-            binding.itemPollOption1Progress.apply {
-                max = 60
-                progress = 10
-            }
-            binding.itemPollOption2Progress.apply {
-                max = 60
-                progress = 20
-            }
-            binding.itemPollOption3Progress.apply {
-                max = 60
-                progress = 30
-            }
-            binding.itemPollOption4Progress.apply {
-                max = 60
-                progress = 40
-            }
-            binding.itemPollOption5Progress.apply {
-                max = 60
-                progress = 50
-            }
-            binding.itemPollOption6Progress.apply {
-                max = 60
-                progress = 60
+        val pollOptionsProgressList = mutableListOf(
+            binding.itemPollOption1Progress,
+            binding.itemPollOption2Progress,
+            binding.itemPollOption3Progress,
+            binding.itemPollOption4Progress,
+            binding.itemPollOption5Progress,
+            binding.itemPollOption6Progress,
+        ).toTypedArray()
+
+        fun bind(binding: ItemDiscussionPollBinding, pollModel: PollModel) {
+            //hiding more options button on discussion polls
+            binding.pollsMoreOptions.visibility = View.GONE
+
+            //setting the profile image of current poll's user
+            Glide.with(itemView.context)
+                .load(pollModel.userImage)
+                .placeholder(R.drawable.ic_profile)
+                .circleCrop()
+                .into(binding.itemPollUserImage)
+
+            //navigating to zoom image activity on clicking profile image
+            binding.itemPollUserImage.setOnClickListener {
+                val context = binding.itemPollUserImage.context
+                val intent = Intent(context, ZoomImageActivity::class.java)
+                intent.putExtra(Constants.ZOOM_IMAGE_URL, pollModel.userImage)
+                context.startActivity(intent)
             }
 
+            binding.itemPollUsername.text = pollModel.username
+
+            val time = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.getDefault())
+                .parse(pollModel.createdAt)
+            binding.itemPollTime.text = time?.let {
+                SimpleDateFormat("dd MMM yyyy", Locale.getDefault())
+                    .format(it)
+            }
+
+            binding.itemPollTitle.apply {
+                text = pollModel.title
+                visibility = if (pollModel.title.isEmpty()) View.GONE else View.VISIBLE
+            }
+            binding.itemPollContent.apply {
+                text = pollModel.content
+                visibility = if (pollModel.content.isEmpty()) View.GONE else View.VISIBLE
+            }
+
+            //setting the poll options
+            val pollOptions = pollModel.pollOptions
+
+            binding.itemPollLikes.text = pollModel.likes.toString()
+            binding.itemPollComments.text = pollModel.comments.toString()
         }
+    }
 
+    class PollsDiffCallback : DiffUtil.ItemCallback<PollModel>() {
+        override fun areItemsTheSame(oldItem: PollModel, newItem: PollModel) =
+            oldItem.pollId == newItem.pollId
+
+        override fun areContentsTheSame(oldItem: PollModel, newItem: PollModel) =
+            oldItem == newItem
     }
 }
--- app/src/main/java/com/example/discussions/ui/home/PollsFragment.kt
+++ app/src/main/java/com/example/discussions/ui/home/PollsFragment.kt
@@ -1,12 +1,13 @@
 package com.example.discussions.ui.home
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.discussions.Constants
 import com.example.discussions.adapters.PollsRecyclerAdapter
 import com.example.discussions.databinding.FragmentPollsBinding
 import com.example.discussions.viewModels.HomeViewModel
@@ -17,6 +18,7 @@ class PollsFragment : Fragment() {
     private lateinit var binding: FragmentPollsBinding
     private lateinit var homeViewModel: HomeViewModel
 
+    private lateinit var pollsAdapter: PollsRecyclerAdapter
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -24,16 +26,54 @@ class PollsFragment : Fragment() {
         binding = FragmentPollsBinding.inflate(inflater, container, false)
         homeViewModel = ViewModelProvider(requireActivity())[HomeViewModel::class.java]
 
-        binding.pollsRv.adapter = PollsRecyclerAdapter()
+        binding.pollsRv.apply {
+            pollsAdapter = PollsRecyclerAdapter()
+            adapter = pollsAdapter
+        }
+
+        binding.pollsSwipeLayout.setOnRefreshListener {
+            homeViewModel.refreshAllUserPolls(
+                requireContext()
+            )
+        }
 
         getAllUserPolls()
         return binding.root
     }
 
     private fun getAllUserPolls() {
+        binding.pollsProgressBar.visibility = View.VISIBLE
         homeViewModel.userPollsList.observe(viewLifecycleOwner) {
             if (it != null) {
-                Log.d(TAG, "getAllUserPolls: $it")
+                pollsAdapter.submitList(it) {
+                    //scroll to top after loading new data
+                    binding.pollsRv.scrollToPosition(0)
+                }
+                //hiding all loading
+                binding.pollsSwipeLayout.isRefreshing = false
+                binding.pollsProgressBar.visibility = View.GONE
+                binding.pollsLottieNoData.visibility = View.GONE
+
+                //when empty list is loaded
+                if (it.isEmpty()) {
+                    binding.pollsLottieNoData.visibility = View.VISIBLE
+                    val error = homeViewModel.isUserPollsFetched.value
+
+                    //when empty list is due to network error
+                    if (error != Constants.API_SUCCESS) {
+                        Toast.makeText(
+                            requireContext(),
+                            homeViewModel.isUserPollsFetched.value,
+                            Toast.LENGTH_SHORT
+                        )
+                            .show()
+                    }
+
+                    if (error == Constants.AUTH_FAILURE_ERROR) {
+                        requireActivity().setResult(Constants.RESULT_LOGOUT)
+                        requireActivity().finish()
+                    }
+                }
             }
         }
 
