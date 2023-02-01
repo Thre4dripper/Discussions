53b9c4ad6178d90851620022a14ff249dea80046
Ijlal Ahmad <ijlalahmad845@gmail.com>
2023-02-01 19:04:33 +0530

    added post details activity

---
+++ app/src/main/java/com/example/discussions/ui/PostDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.discussions.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.discussions.R
+
+class PostDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post_details)
+    }
+}
