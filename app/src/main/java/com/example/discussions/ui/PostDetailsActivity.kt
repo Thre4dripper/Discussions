4b02f6bba96d2ea610a7339acec7de08531e26fd
Ijlal Ahmad <ijlalahmad845@gmail.com>
2023-02-02 16:33:22 +0530

    designed post details activity

--- app/src/main/java/com/example/discussions/ui/PostDetailsActivity.kt
+++ app/src/main/java/com/example/discussions/ui/PostDetailsActivity.kt
@@ -2,11 +2,14 @@ package com.example.discussions.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.example.discussions.R
+import com.example.discussions.databinding.ActivityPostDetailsBinding
 
 class PostDetailsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityPostDetailsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_post_details)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_post_details)
     }
 }
