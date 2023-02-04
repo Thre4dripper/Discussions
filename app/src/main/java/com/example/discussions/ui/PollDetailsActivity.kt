ceb3cbefe7f89885450c15b0ad14c2d225acfdac
Ijlal Ahmad <ijlalahmad845@gmail.com>
2023-02-04 20:29:31 +0530

    added poll detail activity and designed the ui of poll item and comments ui on it

---
+++ app/src/main/java/com/example/discussions/ui/PollDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.discussions.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.discussions.R
+
+class PollDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_poll_details)
+    }
+}
